69b810e4862ca6c886a8b3460e6d661c5595c2da
Abraham Kristanto <abkristanto@gmail.com>
2021-10-12 15:38:53 +0700

    Add login page

---
+++ lib/login_page.dart
@@ -0,0 +1,116 @@
+import 'package:flutter/material.dart';
+
+class LoginPageState extends StatefulWidget {
+  @override
+  _LoginPageStateState createState() => _LoginPageStateState();
+}
+
+class _LoginPageStateState extends State<LoginPageState> {
+  final formKey = new GlobalKey<FormState>();
+
+  String email, password;
+
+  //To check fields during submit
+  checkFields() {
+    final form = formKey.currentState;
+    if (form.validate()) {
+
+    }
+  }
+
+  //To validate email
+  String validateEmail(String value) {
+    Pattern pattern =
+        r'^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$';
+    RegExp regex = new RegExp(pattern);
+    if (!regex.hasMatch(value))
+      return 'Enter Valid Email';
+    else
+      return null;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+      height: MediaQuery.of(context).size.height,
+      width: MediaQuery.of(context).size.width,
+      child: Form(
+        key: formKey,
+        child: _buildLoginForm(),
+      ),
+    ));
+  }
+
+  _buildLoginForm() {
+    return Padding(
+      padding: const EdgeInsets.only(left: 25.0, right: 25.0),
+      child: ListView(
+        children: [
+          SizedBox(
+            height: 75.0,
+          ),
+          Container(
+            height: 125.0,
+            width: 200.0,
+            child: Stack(
+              children: [
+                Text(
+                  'Hello',
+                  style: TextStyle(fontFamily: 'Truneo', fontSize: 60.0),
+                ),
+                Positioned(
+                  top: 50.0,
+                  child: Text(
+                    'There',
+                    style: TextStyle(fontFamily: 'Truneo', fontSize: 60.0),
+                  ),
+                ),
+                Positioned(
+                  top: 97.0,
+                  left: 175.0,
+                  child: Container(
+                    height: 10.0,
+                    width: 10.0,
+                    decoration: BoxDecoration(
+                        shape: BoxShape.circle, color: Colors.amberAccent),
+                  ),
+                ),
+              ],
+            ),
+          ),
+          SizedBox(height: 25.0),
+          TextFormField(
+            decoration: InputDecoration(
+              labelText: 'EMAIL',
+              labelStyle: TextStyle(
+                fontFamily: 'Trueno',
+                fontSize: 12.0,
+                color: Colors.grey.withOpacity(0.5),
+              ),
+            ),
+            onChanged: (value) {
+              this.email = value; 
+            },
+            validator: (value) => value.isEmpty ? 'Email is required':validateEmail(value)
+          ),
+          TextFormField(
+              decoration: InputDecoration(
+                labelText: 'PASSWORD',
+                labelStyle: TextStyle(
+                  fontFamily: 'Trueno',
+                  fontSize: 12.0,
+                  color: Colors.grey.withOpacity(0.5),
+                ),
+              ),
+              onChanged: (value) {
+                this.email = value;
+              },
+              obscureText: true,
+              validator: (value) =>
+                  value.isEmpty ? 'Password is required' : null),
+        ],
+      ),
+    );
+  }
+}
